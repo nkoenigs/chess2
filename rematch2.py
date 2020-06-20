13f4edba2a48dca8658a4cde3a864eac3b2753bc
Nathan Koenigsmark <ndkoenigsmark@gmail.com>
2020-06-20 06:57:27 -0400

    bux fixxing starting minmax

--- rematch2.py
+++ rematch2.py
@@ -5,6 +5,7 @@ import chess.pgn
 import random
 
 'uh oh here here i go again...'
+key_counter = 0
 
 class engine:
     """
@@ -21,7 +22,11 @@ class engine:
     def __init__(self, tlim):
         self.solved_queue = mp.Queue()
         self.unsolved_queue = mp.JoinableQueue()
-        self.active_nodes = []
+        self.leaves = []
+        self.root = None
+        self.node_keys = {}
+        self.alpha = None
+        self.beta = None
 
     def request(self):
         """
@@ -40,15 +45,129 @@ class engine:
     def play(self, board, tlim):
         # some setup
         start_time = time.time()
-        leaves = self.create_root(board)
+        self.create_root(board)
 
-        leaves_copy = leaves.copy()
+        leaves_copy = self.leaves.copy()
         for leaf in leaves_copy:
-            self.grow_branch(leaves, leaf)
+            self.grow_branch(leaf)
 
-        for leaf in leaves:
-            print(leaf.metrics.value)
+        # for leaf in self.leaves:
+        #     print("val: " + str(leaf.metrics.value))
 
+    def send_node_to_workers(self, node):
+        """
+        this function sends data to the queues to evaluate a board
+        """
+        data = pickleable_data(node)
+        self.node_keys[data.key] = node
+        self.unsolved_queue.put(data)
+
+    def collect_work(self):
+        """
+        puts unpickled data into its node
+        """
+        while len(self.node_keys) > 0:
+            if not self.solved_queue.empty():
+                data = self.solved_queue.get()
+                node = self.node_keys.pop(data.key)
+                node.metrics.value = data.value
+                node.metrics.interest = data.interest
+                self.leaves.append(node)
+
+    def create_root(self, board):
+        """
+        makes a root for the tree
+        then grows the first layer
+        """
+        self.root = chess.pgn.Game()
+        self.root.setup(board.fen())
+
+        for move in self.root.board().legal_moves:
+            new_node = self.root.add_variation(move)
+            new_node.metrics = metrics(move)
+            self.send_node_to_workers(new_node)
+
+        self.unsolved_queue.join()
+        self.collect_work()
+
+    def grow_branch(self, parent):
+        """
+        creates child nodes for all avalible moves from a given parent gameNode
+        also modifies the leaves
+        """
+        self.leaves.remove(parent)
+        for move in parent.board().legal_moves:
+            new_node = parent.add_variation(move)
+            new_node.metrics = parent.metrics.childs_metrics()
+            self.send_node_to_workers(new_node)
+
+        self.unsolved_queue.join()
+        self.collect_work()
+
+    # def minmax(self):
+    #     """
+    #     alpha beta mixmaxing
+    #     returns the best move
+    #     """
+    #     self.alpha = -10000000
+    #     self.beta = 10000000
+    #     self.recure(root, self.root.board().turn)
+
+    # def recur(self, loc, is_max):
+    #     """
+    #     im sorry father i have sinned, i had no other option
+    #     forgive me
+    #     """
+
+
+def run(unsolved_queue, solved_queue):
+    """
+    run this as a process in the background
+    use queues to pass in ideas to evaluate
+    """
+    while True:
+        if not unsolved_queue.empty():
+            pkobj = unsolved_queue.get()
+
+            rand = random.randrange(100) - 50
+            ret = unpickleable_data(rand, rand, pkobj.key)
+
+            solved_queue.put(ret)
+            unsolved_queue.task_done()
+
+class metrics:
+    """
+    An idea is complex object desgined to help workers evaluate moves
+    """
+    def __init__(self, move):
+        self.depth = 1
+        self.root_move = move
+        self.value = None
+        self.interest = None
+
+    def childs_metrics(self):
+        """
+        returns updated metrics for my child
+        """
+        childs = metrics(None)
+        childs.depth = self.depth + 1
+        childs.root_move = self.root_move
+        return childs
+
+class pickleable_data:
+    def __init__(self, node):
+        global key_counter
+        self.after_fen =  node.board().fen()
+        self.before_fen = node.parent.board().fen()
+        self.move = node.move 
+        self.key = key_counter
+        key_counter += 1
+
+class unpickleable_data:
+    def __init__(self, value, interest, key):
+        self.key = key
+        self.value = value 
+        self.interest = interest
 
         # # get all legal moves
         # if bool(board.legal_moves) == False:
@@ -75,92 +194,3 @@ class engine:
         #         highest_rated = move
 
         # return highest_rated[0]
-
-    def create_root(self, board):
-        """
-        makes a root for the tree
-        then grows the first layer
-        returns the leaves
-        """
-        root = chess.pgn.Game.from_board(board)
-        leaves = []
-        for move in root.board().legal_moves:
-            new_node = root.add_variation(move)
-            new_node.metrics = metrics(move)
-            self.unsolved_queue.put(new_node)
-            self.active_requests += 1
-        self.unsolved_queue.join()
-
-        while self.active_requests > 0:
-            if not self.solved_queue.empty():
-                leaves.append(self.solved_queue.get())
-                self.active_requests -= 1
-
-        return leaves
-
-    def grow_branch(self, leaves, parent):
-        """
-        creates child nodes for all avalible moves from a given parent gameNode
-        also modifies the leaves
-        returns a list of all new leaves
-        """
-        leaves.remove(parent)
-
-        for move in parent.board().legal_moves:
-            new_node = parent.add_variation(move)
-            new_node.metrics = parent.metrics.childs_metrics()
-            self.unsolved_queue.put(new_node)
-            self.active_requests += 1
-        self.unsolved_queue.join()
-
-        while self.active_requests > 0:
-            if not self.solved_queue.empty():
-                leaves.append(self.solved_queue.get())
-                self.active_requests -= 1        
-
-        return new_leaves
-
-def run(unsolved_queue, solved_queue):
-    """
-    run this as a process in the background
-    use queues to pass in ideas to evaluate
-    """
-    while True:
-        if not unsolved_queue.empty():
-            node = unsolved_queue.get()
-
-            node.metrics.value = random.randrange(100)
-
-            solved_queue.put(node)
-            unsolved_queue.task_done()
-
-
-
-
-class metrics:
-    """
-    An idea is complex object desgined to help workers evaluate moves
-    """
-    def __init__(self, move):
-        self.depth = 1
-        self.root_move = move
-        self.value = None
-        self.interest = None
-
-    def childs_metrics(self):
-        """
-        returns updated metrics for my child
-        """
-        childs = metrics(None)
-        childs.depth = self.depth + 1
-        childs.root_move = self.root_move
-        return childs
-
-class pickleable_data:
-    """
-    a class to enable passing relivant data through a queue
-    """
-    def __init__(self, node, index)
-    self.metrics = node.metrics
-    self.index = index
-    self.after = node.board()
